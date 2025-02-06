46645b9e68c7c214ddc7c94a61c1cecba0ab396a
Artem Rasskazimov <john@WCR.local>
2025-02-07 00:01:15 +0100

    day09, part 2

--- day09.py
+++ day09.py
@@ -2,7 +2,7 @@
 
 # Preparation
 
-from itertools import batched
+from itertools import batched, chain
 
 with open('./inputs/day09.txt', 'r') as file:
     input = file.read().split('\n')[0]
@@ -45,3 +45,39 @@ for upreader in range(len(data)):
     result1 += upreader * data[upreader]
 
 print("Part 1 answer: ", result1)
+
+# Part 2
+
+result2 = 0
+data = []
+
+for id, (file, gap) in enumerate(paired_input):
+    data.append({ 'type': 'file', 'size': file, 'id': id })
+    data.append({ 'type': 'gap', 'size': gap, 'id': 0 })
+
+for pointer in range(len(data) - 1, 0, -1):
+    if data[pointer]['type'] != 'file':
+        continue
+
+    try:
+        writer_index, writer = next((index, x) for index, x in enumerate(data[:pointer]) if x['type'] == 'gap' and x['size'] >= data[pointer]['size'])
+    except StopIteration:
+        continue
+
+    data[pointer]['type'] = 'gap'
+    data[writer_index]['size'] -= data[pointer]['size']
+
+    data.insert(writer_index, { 'type': 'file', 'size': data[pointer]['size'], 'id': data[pointer]['id']})
+
+id_index = 0
+
+for item in data:
+    if item['type'] == 'gap':
+        id_index += item['size']
+        continue
+
+    for i in range(item['size']):
+        result2 += id_index * item['id']
+        id_index += 1
+
+print("Part 2 answer: ", result2)
